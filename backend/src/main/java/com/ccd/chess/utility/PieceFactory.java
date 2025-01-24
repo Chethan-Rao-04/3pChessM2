70b3c776494d21a9cd0b4b4cfe2f0c74b34b2397
Chethan <chethan.chinnabhandara@st.ovgu.de>
2025-01-24 11:26:24 +0100

    Fixed Piece Factory

--- backend/src/main/java/com/ccd/chess/utility/PieceFactory.java
+++ backend/src/main/java/com/ccd/chess/utility/PieceFactory.java
@@ -1,14 +1,16 @@
 package com.ccd.chess.utility;
-import common.Colour;
-import model.BasePiece;
-import model.Bishop;
-import model.Jester;
-import model.King;
-import model.Knight;
-import model.Pawn;
-import model.Queen;
-import model.Rook;
-import model.Wall;
+import com.ccd.chess.entity.enums.Colour;
+import com.ccd.chess.entity.ChessPiece;
+import com.ccd.chess.entity.Bishop;
+import com.ccd.chess.entity.NewPiece1;
+import com.ccd.chess.entity.King;
+import com.ccd.chess.entity.Knight;
+import com.ccd.chess.entity.Pawn;
+import com.ccd.chess.entity.Queen;
+import com.ccd.chess.entity.Rook;
+import com.ccd.chess.entity.NewPiece2;
+
+
 
 /**
  * PieceFactory - helper class to create objects chess pieces
@@ -19,9 +21,9 @@ public class PieceFactory {
      * createPiece - based on the type and colour, creates a chess piece
      * @param colour - piece colour
      * @param type - piece name e.g. king
-     * @return BasePiece
+     * @return ChessPiece
      **/
-    public static BasePiece createPiece(String type, Colour colour) {
+    public static ChessPiece createPiece(String type, Colour colour) {
         switch (type.toLowerCase()) {
             case "bishop":
                 return new Bishop(colour);
@@ -35,10 +37,10 @@ public class PieceFactory {
                 return new Rook(colour);
             case "pawn":
                 return new Pawn(colour);
-            case "jester":
-                return new Jester(colour);
-            case "wall":
-                return new Wall(colour);
+            case "NewPiece1":
+                return new NewPiece1(colour);
+            case "NewPiece2":
+                return new NewPiece2(colour);
             default:
                 throw new IllegalArgumentException("Invalid chess piece type: " + type);
         }
