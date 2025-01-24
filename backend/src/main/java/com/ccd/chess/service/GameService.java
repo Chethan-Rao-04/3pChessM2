f6151f9f67dce3c703e92896af075e4782fd2dad
Chethan <chethan.chinnabhandara@st.ovgu.de>
2025-01-24 13:54:41 +0100

    imports fixed

--- backend/src/main/java/com/ccd/chess/service/GameService.java
+++ backend/src/main/java/com/ccd/chess/service/GameService.java
@@ -18,9 +18,9 @@ import java.util.Set;
 
 import static com.ccd.chess.utility.BoardAdapter.calculatePolygonId;
 
-public class GameService implements IGameInterface {
+public class GameMain implements IGameInterface {
 
-    private static final String TAG = GameService.class.getSimpleName();
+    private static final String TAG = GameMain.class.getSimpleName();
     private final Board board;
     private Position moveStartPos, moveEndPos;
     private Set<Position> highlightPolygons;
@@ -28,7 +28,7 @@ public class GameService implements IGameInterface {
     /**
      * GameMain Constructor. Entry point to the backend logic
      * */
-    public GameService() {
+    public GameMain() {
         Logger.d(TAG, "initGame GameMain()");
         board = new Board();
         moveStartPos = null;
