9eee249c2393d8ce79e551242301703f50d84e36
bhaveshY <byeole80@gmail.com>
2025-01-28 16:54:19 +0100

    fixed and added dataprovider

---
+++ backend/src/test/java/com/ccd/chess/test/DataProvider.java
@@ -0,0 +1,43 @@
+package com.ccd.chess.test;
+
+import com.ccd.chess.entity.*;
+import com.ccd.chess.entity.enums.Colour;
+
+import java.util.stream.Stream;
+
+/**
+ * Provides test data for parameterized tests.
+ */
+public class DataProvider {
+
+    /**
+     * Provides a stream of chess pieces for testing.
+     * Returns pieces of all colors (BLUE, GREEN, RED) for comprehensive testing.
+     * @return Stream of chess pieces
+     */
+    public static Stream<ChessPiece> pieceProvider() {
+        return Stream.of(
+            // Blue pieces
+            new Pawn(Colour.BLUE),
+            new Rook(Colour.BLUE),
+            new Knight(Colour.BLUE),
+            new Bishop(Colour.BLUE),
+            new Queen(Colour.BLUE),
+            new King(Colour.BLUE),
+            // Green pieces
+            new Pawn(Colour.GREEN),
+            new Rook(Colour.GREEN),
+            new Knight(Colour.GREEN),
+            new Bishop(Colour.GREEN),
+            new Queen(Colour.GREEN),
+            new King(Colour.GREEN),
+            // Red pieces
+            new Pawn(Colour.RED),
+            new Rook(Colour.RED),
+            new Knight(Colour.RED),
+            new Bishop(Colour.RED),
+            new Queen(Colour.RED),
+            new King(Colour.RED)
+        );
+    }
+} 
